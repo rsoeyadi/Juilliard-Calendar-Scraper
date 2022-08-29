93819a16205fc2b953c176013697293ba221b808
Ryan Soeyadi <rs4163@columbia.edu>
2022-08-28 20:59:43 -0400

    dashes are now allowed for 'pre-college'

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def add_keyword():
 
         for kw in curr_list:
             # regex to remove special characters except colon
-            kw = re.sub(r'[^a-zA-Z0-9:]+', '', kw)
+            kw = re.sub(r'[^a-zA-Z0-9:-]+', '', kw)
             if kw:  # if sanitized keyword is not empty
                 kw = kw.lower()
                 if kw not in keywords:
