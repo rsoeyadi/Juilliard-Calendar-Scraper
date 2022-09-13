2830df69f0bf91cc289a685948449dbf340571a2
Ryan Soeyadi <rs4163@columbia.edu>
2022-09-13 17:44:15 -0400

    remove print

--- scrape.py
+++ scrape.py
@@ -50,7 +50,6 @@ def get_events():
                     time2 = time2.replace(":00", "")
                 day_of_week = date_time.strftime('%A') # for searching db by day of week in app
                 title = e.find('div', {'class': 'title-subtitle'}).text
-                print(title)
                 venue = e.find(
                     'div', {'class': 'field--name-field-venue field__item'}).text if e.find('div', {'class': 'field--name-field-venue field__item'}) else ''
                 tags = e.find('div', {'class': 'field--name-field-event-tags'}).text.split('\n')
