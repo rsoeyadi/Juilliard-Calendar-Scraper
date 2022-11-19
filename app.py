a16b1e4074ab18c5cce076e0882078188d9a2bdc
Ryan Soeyadi <rs4163@columbia.edu>
2022-11-18 23:54:30 -0500

    changed timedelta

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ def search_db():
                     str(event[4]), # tags
                     str(event[12]), # link
                     datetime.strptime(event[1], "%Y-%m-%d %H:%M:%S").time().strftime("%I:%M %p").lstrip('0'), 
-                    str(datetime.strptime(event[1], "%Y-%m-%d %H:%M:%S").date()) == str((datetime.today() - timedelta(hours=3)).strftime("%Y-%m-%d")), 
+                    str(datetime.strptime(event[1], "%Y-%m-%d %H:%M:%S").date()) == str((datetime.today() - timedelta(hours=5)).strftime("%Y-%m-%d")), 
                     event[13]) 
                 for event in results]
     
